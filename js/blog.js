2210b934c82cc15b41e42f7038393c76ab06d4e3
send-bee <betamotoe5@gmail.com>
2025-04-07 14:46:28 -0500

    efecto del carrucel

--- js/blog.js
+++ js/blog.js
@@ -2,13 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const carouselContainer = document.querySelector(".carousel-container");
     let index = 0;
     function slideCarousel() {
+        carouselContainer.style.transition = "none";
         index++;
-        if (index >= carouselContainer.children.length) {
+        if (index >= 1230) {
             index = 0;
         }
-        carouselContainer.style.transform = `translateX(-${index * 100}%)`;
+        carouselContainer.style.transform = `translateX(-${index * 0.1}%)`;
     }
-    setInterval(slideCarousel, 3000);
+    setInterval(slideCarousel, 10);
     const buttons = document.querySelectorAll(".btn");
 
     buttons.forEach(button => {
